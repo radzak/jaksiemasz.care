98d63529435a673526bed4a2366092b48ed7f4c9
Radek Krzak <radek.krzak@gmail.com>
2017-11-27 22:31:23 +0100

    Make Person class immutable

--- src/main/scala/faker/Person.scala
+++ src/main/scala/faker/Person.scala
@@ -1,31 +1,14 @@
 package faker
 
 class Person(
-              private var _name: String,
-              private var _surname: String,
-              private var _email: String) {
-  name = _name
-  surname = _surname
-  email = _email
+              val name: String,
+              val surname: String,
+              val email: String) {
 
-  def name: String = _name
-  def name_= (newName: String): Unit = {
-    require(newName.matches("[A-Z][a-z]+"), "Wrong name!")
-    _name = newName
-  }
-
-  def surname: String = _surname
-  def surname_= (newSurname: String): Unit = {
-    require(newSurname.matches("[A-Z][a-z]+"), "Wrong surname!")
-    _surname = newSurname
-  }
-
-  def email: String = _email
-  def email_= (newEmail: String): Unit = {
-    val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
-    require(newEmail.matches(emailRegex), "Wrong email!")
-    _email = email
-  }
+  val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+  require(name.matches("[A-Z][a-z]+"), "Wrong name!")
+  require(surname.matches("[A-Z][a-z]+"), "Wrong surname!")
+  require(email.matches(emailRegex), "Wrong email!")
 
   override def toString: String = s"Person<$name $surname, email: $email>"
 
